4b29010409993641f959e4a573c1e2ae5b82a7f2
Maria Revelo <fer_nandita_8@hotmail.com>
2023-11-12 12:46:37 -0800

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import countries from './data/countries'
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App;
+
---
+++ src/data/countries.js
@@ -0,0 +1,154 @@
+const countries =[
+    {
+        "Country": "Germany",
+        "Capital": "Berlin",
+        "imgFlag": "/images/germany.png"
+    },
+    {
+        "Country": "France",
+        "Capital": "Paris",
+        "imgFlag": "/images/france.png"
+    },
+    {
+        "Country": "Italy",
+        "Capital": "Rome",
+        "imgFlag": "/images/france.png"
+    },
+    {
+        "Country": "Spain",
+        "Capital": "Madrid",
+        "imgFlag": "/images/spain.png"
+    },
+    {
+        "Country": "United Kingdom",
+        "Capital": "London",
+        "imgFlag": "/images/UK.png"
+    },
+    {
+        "Country": "Netherlands",
+        "Capital": "Amsterdam",
+        "imgFlag": "/images/netherlands.png"
+    },
+    {
+        "Country": "Belgium",
+        "Capital": "Brussels",
+        "imgFlag": "/images/belgium.png"
+    },
+    {
+        "Country": "Sweden",
+        "Capital": "Stockholm",
+        "imgFlag": "/images/sweden.png"
+    },
+    {
+        "Country": "Norway",
+        "Capital": "Oslo",
+        "imgFlag": "/images/norway.png"
+    },
+    {
+        "Country": "Poland",
+        "Capital": "Warsaw",
+        "imgFlag": "/images/poland.png"
+    },
+    {
+        "Country": "China",
+        "Capital": "Beijing",
+        "imgFlag": "/images/china.png"
+    },
+    {
+        "Country": "India",
+        "Capital": "New Delhi",
+        "imgFlag": "/images/india.png"
+    },
+    {
+        "Country": "Japan",
+        "Capital": "Tokyo",
+        "imgFlag": "/images/japan.png"
+    },
+    {
+        "Country": "South Korea",
+        "Capital": "Seoul",
+        "imgFlag": "/images/south-korea.png"
+    },
+    {
+        "Country": "Russia",
+        "Capital": "Moscow",
+        "imgFlag": "/images/russia.png"
+    },
+    {
+        "Country": "Indonesia",
+        "Capital": "Jakarta",
+        "imgFlag": "/images/indonesia.png"
+    },
+    {
+        "Country": "Thailand",
+        "Capital": "Bangkok",
+        "imgFlag": "/images/thailand.png"
+    },
+    {
+        "Country": "Vietnam",
+        "Capital": "Hanoi",
+        "imgFlag": "/images/vietnam.png"
+    },
+    {
+        "Country": "Malaysia",
+        "Capital": "Kuala Lumpur",
+        "imgFlag": "/images/malaysia.png"
+    },
+    {
+        "Country": "Singapore",
+        "Capital": "Singapore",
+        "imgFlag": "/images/singapore.png"
+    },
+    {
+        "Country": "United States",
+        "Capital": "Washington D.C.",
+        "imgFlag": "/images/usa.png"
+    },
+    {
+        "Country": "Canada",
+        "Capital": "Ottawa",
+        "imgFlag": "/images/canada.png"
+    },
+    {
+        "Country": "Mexico",
+        "Capital": "Mexico City",
+        "imgFlag": "/images/mexico.png"
+    },
+    {
+        "Country": "Guatemala",
+        "Capital": "Guatemala City",
+        "imgFlag": "/images/guatemala.png"
+    },
+    {
+        "Country": "Costa Rica",
+        "Capital": "San José",
+        "imgFlag": "/images/costarica.png"
+    },
+    {
+        "Country": "Panama",
+        "Capital": "Panama City",
+        "imgFlag": "/images/panama.png"
+    },
+    {
+        "Country": "Brazil",
+        "Capital": "Brasília",
+        "imgFlag": "/images/brazil.png"
+    },
+    {
+        "Country": "Argentina",
+        "Capital": "Buenos Aires",
+        "imgFlag": "/images/argentina.png"
+    },
+    {
+        "Country": "Chile",
+        "Capital": "Santiago",
+        "imgFlag": "/images/chile.png"
+    },
+    {
+        "Country": "Peru",
+        "Capital": "Lima",
+        "imgFlag": "/images/peru.png"
+    }
+]
+
+export default countries;
